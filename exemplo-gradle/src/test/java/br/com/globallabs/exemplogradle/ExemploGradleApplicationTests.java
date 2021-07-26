a89576e8f1c1969e79faab357dc7494c7a188297
Erismar Oliveira <erismarpro@hotmail.com>
2021-07-26 18:53:36 -0300

    Exemplo spring com gradle

---
+++ exemplo-gradle/src/test/java/br/com/globallabs/exemplogradle/ExemploGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.globallabs.exemplogradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemploGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
